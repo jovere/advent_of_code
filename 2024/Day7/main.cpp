327b53d10ae823199219113196906c8bb33c3740
Jeremy Overesch <10420943+jovere@users.noreply.github.com>
2024-12-08 22:27:52 -0600

    Advent-of-Code-2024-Day7 Attempt 1, test data works, but real result is wrong.

--- 2024/Day7/main.cpp
+++ 2024/Day7/main.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <vector>
+#include <numeric>
 #include <unordered_map>
 #include <fmt/format.h>
 #include <fmt/ranges.h>
@@ -8,7 +9,7 @@
 int
 main()
 {
-    std::fstream input("input_test.txt", std::ios::in);
+    std::fstream input("input.txt", std::ios::in);
     if (!input.is_open())
     {
         fmt::print(stderr, "Can't open file.");
@@ -18,12 +19,12 @@ main()
     std::string line;
 
     // Get all the order rules from the file in a map
-    std::unordered_map<long, std::vector<long>> calibrationSet;
+    std::unordered_map<uint64_t, std::vector<uint64_t>> calibrationSet;
     while (getline(input, line) && !line.empty())
     {
         std::vector<std::string> result;
         boost::split(result, line, boost::is_any_of(":"));
-        std::vector<long> operands;
+        std::vector<uint64_t> operands;
         {
             std::vector<std::string> strOperands;
             boost::trim(result[1]);
@@ -37,8 +38,45 @@ main()
         calibrationSet.insert({std::stol(result[0]), operands});
     }
 
-    fmt::print("Map: {}", calibrationSet);
+    fmt::print("Map: {}\n", calibrationSet);
 
+    uint64_t sumTotal = 0;
+    for(auto const& cal: calibrationSet)
+    {
+        auto& expectedTotal = cal.first;
+        auto& data = cal.second;
+        auto operatorCount = data.size()-1;
+        uint64_t operatorCombinations = 1UL << operatorCount;
 
+        fmt::print("Operator count of {} is {}. ", data, operatorCount);
+        fmt::print("Possible operator combinations is {}. ", operatorCombinations);
+        fmt::print("Desired Total is {}.\n", expectedTotal);
+
+        bool match = false;
+        do
+        {
+            --operatorCombinations;
+            auto combo = operatorCombinations;
+            uint64_t total = std::accumulate(std::next(data.begin()), data.end(), data[0], [&combo](uint64_t a, uint64_t b)
+            {
+                uint64_t retVal = 0;
+                if((combo & 0x1) == 0x1)
+                {
+                    retVal = a * b;
+                }
+                else
+                {
+                    retVal = a + b;
+                }
+                combo /= 2;
+                return retVal;
+            });
+            match |= total == expectedTotal;
+            fmt::print("Operations {:0{}b} is {}. {}\n", operatorCombinations, operatorCount, total, total == expectedTotal ? "MATCH" : "");
+        }while(operatorCombinations != 0);// && !match);
+        sumTotal += (match) ? expectedTotal : 0;
+    }
+
+    fmt::print("Total calibration result: {}\n", sumTotal);
     return 0;
 }
