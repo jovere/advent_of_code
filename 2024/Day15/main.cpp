7dcc1402f45ee28ff9e4a1adf84e9089f15eea13
Jeremy Overesch <10420943+jovere@users.noreply.github.com>
2024-12-22 00:48:52 -0600

    Advent-of-Code Day15-2

--- 2024/Day15/main.cpp
+++ 2024/Day15/main.cpp
@@ -19,41 +19,106 @@ printGrid(boost::multi_array<char, 2> const &grid) {
     }
 }
 
+std::optional<std::vector<std::tuple<int, int, int, int>>>
+canMove(boost::multi_array<char, 2> const &grid, std::tuple<int, int> position, std::vector<std::tuple<int, int>> &prev,
+        char direction) {
+    std::vector<std::tuple<int, int, int, int>> vec;
+    auto [row, col] = position;
+    if (auto f = std::find(prev.begin(), prev.end(), position); f != prev.end()) {
+        return vec;
+    }
+    prev.push_back(position);
+    if (grid[row][col] == '#') { return {}; }
+    if (grid[row][col] == '.') {
+        return vec;
+    }
+    if (int rowOffset = (direction == '^') ? -1 : (direction == 'v') ? 1 : 0; rowOffset != 0) {
+        auto v1 = canMove(grid, {row + rowOffset, col}, prev, direction);
+        if (v1 && grid[row][col] == '[') {
+            auto v2 = canMove(grid, {row, col + 1}, prev, direction);
+            if (v2 && v1) {
+                vec.insert(vec.end(), v1->begin(), v1->end());
+                vec.insert(vec.end(), v2->begin(), v2->end());
+                vec.emplace_back(row, col, (direction == '^') ? row - 1 : row + 1, col);
+                return vec;
+            }
+        } else if (v1 && grid[row][col] == ']') {
+            auto v2 = canMove(grid, {row, col - 1}, prev, direction);
+            if (v2 && v1) {
+                vec.insert(vec.end(), v1->begin(), v1->end());
+                vec.insert(vec.end(), v2->begin(), v2->end());
+                vec.emplace_back(row, col, (direction == '^') ? row - 1 : row + 1, col);
+                return vec;
+            }
+        }
+    }
+    return {};
+}
+
 std::optional<std::tuple<int, int>>
-moveRobot(boost::multi_array<char, 2> &grid, std::tuple<int, int> position, char direction) {
+moveRobot(boost::multi_array<char, 2> &grid, std::tuple<int, int> position, char direction, char previous = ' ',
+          bool moving = false) {
     auto [row, col] = position;
     if (grid[row][col] == '#') { return {}; }
     if (grid[row][col] == '.') { return position; }
-    if (grid[row][col] == 'O' || grid[row][col] == '@') {
-        switch (direction) {
-            case '^':
+    switch (direction) {
+        case '^':
+            if ((grid[row][col] == '[' || grid[row][col] == ']')) {
+                std::vector<std::tuple<int, int>> prev;
+                if (auto result = canMove(grid, {row, col}, prev, direction)) {
+                    //std::ranges::sort(result.value());
+                    //std::ranges::unique(result.value());
+                    for (auto [r1, c1, r2, c2]: result.value()) {
+                        std::swap(grid[r1][c1], grid[r2][c2]);
+                    }
+                    return std::tuple{row - 1, col};
+                }
+            } else if (grid[row][col] == '@') {
                 if (moveRobot(grid, {row - 1, col}, direction)) {
                     std::swap(grid[row][col], grid[row - 1][col]);
                     return std::tuple{row - 1, col};
                 }
-                break;
-            case 'v':
+            }
+            break;
+        case 'v':
+            if ((grid[row][col] == '[' || grid[row][col] == ']')) {
+                std::vector<std::tuple<int, int>> prev;
+                if (auto result = canMove(grid, {row, col}, prev, direction)) {
+                    //std::ranges::sort(result.value());
+                    //std::ranges::reverse(result.value());
+                    //std::ranges::unique(result.value());
+                    for (auto [r1, c1, r2, c2]: result.value()) {
+                        std::swap(grid[r1][c1], grid[r2][c2]);
+                    }
+                    return std::tuple{row + 1, col};
+                }
+            } else if (grid[row][col] == '@') {
                 if (moveRobot(grid, {row + 1, col}, direction)) {
                     std::swap(grid[row][col], grid[row + 1][col]);
                     return std::tuple{row + 1, col};
                 }
-                break;
-            case '>':
+            }
+            break;
+        case '>':
+            if (grid[row][col] == '[' || grid[row][col] == ']' || grid[row][col] == '@') {
                 if (moveRobot(grid, {row, col + 1}, direction)) {
                     std::swap(grid[row][col], grid[row][col + 1]);
                     return std::tuple{row, col + 1};
                 }
-                break;
-            case '<':
+            }
+            break;
+        case '<':
+            if (grid[row][col] == '[' || grid[row][col] == ']' || grid[row][col] == '@') {
                 if (moveRobot(grid, {row, col - 1}, direction)) {
                     std::swap(grid[row][col], grid[row][col - 1]);
                     return std::tuple{row, col - 1};
                 }
-                break;
-            default:
-                abort();
-        }
+            }
+            break;
+        default:
+            abort();
     }
+
     return {};
 }
 
@@ -83,14 +148,30 @@ main() {
         }
     }
 
-    fmt::print("Width: {}\n", lines.at(0).size());
+    fmt::print("Width: {}\n", lines.at(0).size() * 2);
     fmt::print("Height: {}\n", lines.size());
 
     using range = boost::multi_array_types::index_range;
-    boost::multi_array<char, 2> grid{boost::extents[lines.at(0).size()][lines.size()]};
+    boost::multi_array<char, 2> grid{boost::extents[lines.at(0).size()][lines.size() * 2]};
 
     for (int i = 0; i < lines.size(); ++i) {
-        std::ranges::copy(lines[i], grid[i].begin());
+        std::vector<char> doubleWide;
+        for (auto object: lines[i]) {
+            switch (object) {
+                case 'O':
+                    doubleWide.push_back('[');
+                    doubleWide.push_back(']');
+                    break;
+                case '@':
+                    doubleWide.push_back('@');
+                    doubleWide.push_back('.');
+                    break;
+                default:
+                    doubleWide.push_back(object);
+                    doubleWide.push_back(object);
+            }
+        }
+        std::ranges::copy(doubleWide, grid[i].begin());
     }
 
     printGrid(grid);
@@ -99,21 +180,21 @@ main() {
     // Find the starting position
     auto start = std::find(multi_array_helper::begin(grid), multi_array_helper::end(grid), '@');
 
-    std::tuple<int,int> position{start.get_indices()[0],start.get_indices()[1]};
+    std::tuple<int, int> position{start.get_indices()[0], start.get_indices()[1]};
     for (auto move: moves) {
-        if(auto newPosition = moveRobot(grid,position,move))
-        {
+        if (auto newPosition = moveRobot(grid, position, move)) {
             position = newPosition.value();
         }
     }
     printGrid(grid);
 
     long gpsSum = 0;
-    for (auto itr = std::find(multi_array_helper::begin(grid), multi_array_helper::end(grid), 'O');
+    for (auto itr = std::find(multi_array_helper::begin(grid), multi_array_helper::end(grid), '[');
          itr != multi_array_helper::end(grid);
-         itr = std::find(++itr, multi_array_helper::end(grid), 'O')) {
+         itr = std::find(++itr, multi_array_helper::end(grid), '[')) {
         gpsSum += itr.get_indices()[0]*100 + itr.get_indices()[1];
     }
     fmt::print("GPS Sum: {}\n", gpsSum);
+
     return 0;
 }
